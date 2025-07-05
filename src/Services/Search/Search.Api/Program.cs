93c618c1eb05014a51ce6124af4836e3bee0b571
raminneynava <raminneynava@gmail.com>
2025-07-05 20:46:19 +0330

    update search

--- src/Services/Search/Search.Api/Program.cs
+++ src/Services/Search/Search.Api/Program.cs
@@ -1,4 +1,4 @@
-using Elastic.Clients.Elasticsearch;
+﻿using Elastic.Clients.Elasticsearch;
 
 using Microsoft.AspNetCore.Http.HttpResults;
 
@@ -21,6 +21,11 @@ app.UseSwaggerUI();
 
 
 app.MapGet("/api/v1/Fuzzy", SearchFuzzyItems);
+app.MapGet("/api/v1/Proximity", SearchProximityItems);
+app.MapGet("/api/v1/Term", SearchTermItems);
+app.MapGet("/api/v1/FullText", SearchFullTextItems);
+app.MapGet("/api/v1/MultiMatch", SearchMultiMatchItems);
+app.MapGet("/api/v1/Boolean", SearchBooleanItems);
 
 app.Run();
 
@@ -45,4 +50,99 @@ static async Task<Results<Ok<IReadOnlyCollection<CatalogItemIndex>>, NotFound>> 
 
     return TypedResults.NotFound();
 
+}
+static async Task<Results<Ok<IReadOnlyCollection<CatalogItemIndex>>, NotFound>> SearchProximityItems(string qr, ElasticsearchClient elasticsearch)
+{
+    var response = await elasticsearch.SearchAsync<CatalogItemIndex>(s => s
+        .Index(CatalogItemIndex.IndexName)
+        .From(0)
+        .Size(10)
+        .Query(q => q
+            .MatchPhrase(mp => mp
+                .Field(f => f.Description)
+                .Query(qr)
+                .Slop(2) // فاصله مجاز بین کلمات، مثلا 2 کلمه
+            )
+        )
+    );
+
+    if (response.IsValidResponse)
+        return TypedResults.Ok(response.Documents);
+
+    return TypedResults.NotFound();
+}
+static async Task<Results<Ok<IReadOnlyCollection<CatalogItemIndex>>, NotFound>> SearchTermItems(string term, ElasticsearchClient elasticsearch)
+{
+    var response = await elasticsearch.SearchAsync<CatalogItemIndex>(s => s
+        .Index(CatalogItemIndex.IndexName)
+        .From(0)
+        .Size(10)
+        .Query(q => q
+            .Term(t => t.Field(f => f.Name).Value(term))
+        )
+    );
+
+    if (response.IsValidResponse)
+        return TypedResults.Ok(response.Documents);
+
+    return TypedResults.NotFound();
+}
+static async Task<Results<Ok<IReadOnlyCollection<CatalogItemIndex>>, NotFound>> SearchFullTextItems(string qr, ElasticsearchClient elasticsearch)
+{
+    var response = await elasticsearch.SearchAsync<CatalogItemIndex>(s => s
+        .Index(CatalogItemIndex.IndexName)
+        .From(0)
+        .Size(10)
+        .Query(q => q
+            .Match(m => m
+                .Field(f => f.Description)
+                .Query(qr)
+            )
+        )
+    );
+
+    if (response.IsValidResponse)
+        return TypedResults.Ok(response.Documents);
+
+    return TypedResults.NotFound();
+}
+static async Task<Results<Ok<IReadOnlyCollection<CatalogItemIndex>>, NotFound>> SearchMultiMatchItems(string qr, ElasticsearchClient elasticsearch)
+{
+    var response = await elasticsearch.SearchAsync<CatalogItemIndex>(s => s
+        .Index(CatalogItemIndex.IndexName)
+        .From(0)
+        .Size(10)
+        .Query(q => q
+            .MultiMatch(mm => mm
+                .Fields("name").Fields("Description")
+                .Query(qr)
+            )
+        )
+    );
+
+    if (response.IsValidResponse)
+        return TypedResults.Ok(response.Documents);
+
+    return TypedResults.NotFound();
+}
+static async Task<Results<Ok<IReadOnlyCollection<CatalogItemIndex>>, NotFound>> SearchBooleanItems(string qrName, string qrDescription, ElasticsearchClient elasticsearch)
+{
+    var response = await elasticsearch.SearchAsync<CatalogItemIndex>(s => s
+        .Index(CatalogItemIndex.IndexName)
+        .From(0)
+        .Size(10)
+        .Query(q => q
+            .Bool(b => b
+                .Must(
+                    mu => mu.Match(m => m.Field(f => f.Name).Query(qrName)),
+                    mu => mu.Match(m => m.Field(f => f.Description).Query(qrDescription))
+                )
+            )
+        )
+    );
+
+    if (response.IsValidResponse)
+        return TypedResults.Ok(response.Documents);
+
+    return TypedResults.NotFound();
 }
